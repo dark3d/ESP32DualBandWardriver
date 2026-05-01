2324672825e366c96d069e7d25a0086d2dcb053b
TheRob-N <robert@neyhard.com>
2026-05-01 13:20:55 -0400

    Fix: remove ieee80211_raw_frame_sanity_check duplicate for ESP32 SDK v3.x compatibility

--- src/WiFiOps.cpp
+++ src/WiFiOps.cpp
@@ -75,13 +75,6 @@ enum MsgType : uint8_t {
 
 WebServer server(80);
 
-// Add compiler.c.elf.extra_flags=-Wl,-zmuldefs to platform.txt
-extern "C" int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3){
-  if (arg == 31337)
-    return 1;
-  else
-    return 0;
-}
 
 class scanCallbacks : public NimBLEScanCallbacks {
 
